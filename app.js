52ea62711f40ffbf18b82766d4ac42099ff464ec
bkay-stack <kevinbest1900@gmail.com>
2024-03-10 07:46:23 +0100

    new

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fetchAdvice = async () => {
     const res = await fetch("https://api.adviceslip.com/advice")
     const data = await res.json()
-  
+   
     document.querySelector("#advice p").innerHTML = `Advice #${data.slip.id}`
     document.querySelector(".advice-content").innerHTML = `"${data.slip.advice}"`
   }
